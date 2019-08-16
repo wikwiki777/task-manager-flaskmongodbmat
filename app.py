c7668032924c53202c1f197a673fc0cc0ffc4844
Ubuntu <wicky.akloe@gmail.com>
2019-08-16 12:23:00 +0000

    Change to os to get IP

--- app.py
+++ app.py
@@ -19,6 +19,6 @@ def get_tasks():
     
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",
+    app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
