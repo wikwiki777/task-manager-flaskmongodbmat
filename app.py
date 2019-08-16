a9a4a95c6102c8149d44a8021c26795838990a8f
Ubuntu <ubuntu@ip-172-31-90-163.ec2.internal>
2019-08-16 11:52:09 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hellow World ..again"
+    
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",
+            port=int(os.environ.get("PORT")),
+            debug=True)
