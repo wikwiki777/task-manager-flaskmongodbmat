846c2e8ffefb99e27ba0b296551f21c47ca13d51
Ubuntu <wicky.akloe@gmail.com>
2019-08-19 15:47:23 +0000

    Add ability to add a category

--- app.py
+++ app.py
@@ -70,6 +70,19 @@ def update_category(category_id):
 def delete_category(category_id):
     mongo.db.categories.remove({"_id": ObjectId(category_id)})
     return redirect(url_for("get_categories"))
+    
+@app.route("/insert_category", methods=["POSt"])
+def insert_category():
+    categories = mongo.db.categories
+    category_doc = {"category_name": request.form.get("category_name")}
+    categories.insert_one(category_doc)
+    return redirect(url_for("get_categories"))
+    
+@app.route("/add_category")
+def add_category():
+    return render_template("addcategory.html")
+    
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0",
             port=int(os.environ.get("PORT")),
